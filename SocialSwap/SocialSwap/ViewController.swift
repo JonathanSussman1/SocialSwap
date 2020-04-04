2957fe28fad3a7837514defc827e9b37a8782ac5
Daye Jack <ddj231@nyu.edu>
2020-04-04 19:12:53 -0400

    initial app commit

---
+++ SocialSwap/SocialSwap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialSwap
+//
+//  Created by DAYE JACK on 4/4/20.
+//  Copyright © 2020 DAYE JACK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
