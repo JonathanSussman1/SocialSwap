c487f8d0f44cc1d63d6bc4f9633d241ad24961e0
Ashley Nussbaum <amn493@nyu.edu>
2020-04-25 21:02:33 -0400

    Change follow button titles, add some two way swap logic

--- SocialSwap/SocialSwap/FollowViewController.swift
+++ SocialSwap/SocialSwap/FollowViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class FollowViewController: UIViewController {
     
+    //buttons
+    @IBOutlet weak var instagramButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var snapchatButton: UIButton!
+    @IBOutlet weak var twitterButton: UIButton!
+    @IBOutlet weak var contactsButton: UIButton!
+    
+    
     var scannedUser: User?
     var currentUser: User?
     var csvForSwap: String?
     var sessionStarter: SessionStarterDelegate?
+    
+    //vars for follow back
+    var instagramPressed: Bool = false
+    var facebookPressed: Bool = false
+    var snapchatPressed: Bool = false
+    var twitterPressed: Bool = false
+    var contactsPressed: Bool = false
+    
+    var sendFollowBackNotification: Bool = false
     
 
     override func viewDidLoad() {
@@ -43,21 +60,26 @@ class FollowViewController: UIViewController {
     
     @IBAction func instagramPressed(_ sender: Any) {
         SwapHelper.openInstagram(handle: "Google")
+        instagramButton.setTitle("Followed", for: UIControl.State.normal)
     }
     
     @IBAction func facebookPressed(_ sender: Any) {
         SwapHelper.openFacebook(url: "facebook.com/Google/")
+        facebookButton.setTitle("Added", for: UIControl.State.normal)
     }
     
     @IBAction func snapchatPressed(_ sender: Any) {
         SwapHelper.openSnapchat(handle: "Google")
+        snapchatButton.setTitle("Added", for: UIControl.State.normal)
     }
     
     @IBAction func twitterPressed(_ sender: Any) {
         SwapHelper.openTwitter(handle: "Google")
+        twitterButton.setTitle("Followed", for: UIControl.State.normal)
     }
     
     @IBAction func contactsPressed(_ sender: Any) {
         SwapHelper.saveContact(firstName: "SocialSwap", lastName: "Testing", phoneNumber: "800111222")
+        contactsButton.setTitle("Added", for: UIControl.State.normal)
     }
 }
--- SocialSwap/SocialSwap/ScannerViewController.swift
+++ SocialSwap/SocialSwap/ScannerViewController.swift
@@ -64,12 +64,20 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
 
     }
 
-    /*
+    
     // MARK: - Navigation
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let followvc: FollowViewController = segue.destination as! FollowViewController
+        //UNCOMMENT WHEN USER IS ADDED
+        //if currentUser.twoWaySwap {
+            followvc.sendFollowBackNotification = true
+        //}
+        //else {
+            //followvc.sendFollowBackNotification = false
+        //}
     }
-    */
+    
 }
