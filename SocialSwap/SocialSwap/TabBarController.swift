a6e4e4014675593d3a805c508da143baa627d772
Daye Jack <ddj231@nyu.edu>
2020-05-01 14:12:52 -0400

    hide passwords, use correct phonepad for phone number text field

--- SocialSwap/SocialSwap/TabBarController.swift
+++ SocialSwap/SocialSwap/TabBarController.swift
@@ -1,29 +1,29 @@
  //
-//  TabBarController.swift
-//  SocialSwap
-//
-//  Created by Ashley Nussbaum on 4/21/20.
-//  Copyright © 2020 Daye Jack, Ashley Nussbaum, Jonathan Sussman. All rights reserved.
-//
-
-import UIKit
-import Firebase
-import FirebaseFirestore
-import FirebaseAuth
-
+ //  TabBarController.swift
+ //  SocialSwap
+ //
+ //  Created by Ashley Nussbaum on 4/21/20.
+ //  Copyright © 2020 Daye Jack, Ashley Nussbaum, Jonathan Sussman. All rights reserved.
+ //
+ 
+ import UIKit
+ import Firebase
+ import FirebaseFirestore
+ import FirebaseAuth
+ 
  class TabBarController: UITabBarController, UITabBarControllerDelegate {
     var user = User()
     var dbloaded=false
-
+    
     /*
-    - (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController {
-
-        if (preventTabChange)
-            return NO;
-
-        return YES;
-    }
-    */
+     - (BOOL)tabBarController:(UITabBarController *)tabBarController shouldSelectViewController:(UIViewController *)viewController {
+     
+     if (preventTabChange)
+     return NO;
+     
+     return YES;
+     }
+     */
     
     func tabBarController(_ tabBarController: UITabBarController,
                           shouldSelect viewController: UIViewController) -> Bool {
@@ -38,70 +38,71 @@ import FirebaseAuth
     }
     
     func getSignedInUser(completion:@escaping((User?) -> ())) {
-
-            let db = Firestore.firestore()
-           _ = db.collection("users").document(String(Auth.auth().currentUser!.uid)).getDocument { (document, error) in
-                 if let document = document, document.exists {
-                    let uemail = document.data()?["email"] as! String
-                    let ufb = document.data()?["facebook"] as! String
-                    let ufirstname = document.data()?["firstName"] as! String
-                    let uid = document.data()?["id"] as! String
-                    let uinstagram = document.data()?["instagram"] as! String
-                    let ulastname = document.data()?["lastName"] as! String
-                    let uphonenumber = document.data()?["phoneNumber"] as! String
-                    let usnapchat = document.data()?["snapchat"] as! String
-                    let utwitter = document.data()?["twitter"] as! String
-                    let utwowayswap = document.data()?["twoWaySwap"] as! Bool
-                    let uswapreceives = document.data()?["userNamesOfSwapRecieves"] as! [String]
-                   self.user = User(uid: uid, firstName: ufirstname, lastName: ulastname, email: uemail, phoneNumber: uphonenumber, twitter: utwitter, instagram: uinstagram, facebook: ufb, snapchat: usnapchat, twoWaySwap: utwowayswap, userNamesOfSwapRecieves: uswapreceives)
-                   completion(self.user)
-                   self.viewDidLoad()
-                 } else {
-                    print("Error getting user")
-                    completion(nil)
-                }
+        
+        let db = Firestore.firestore()
+        _ = db.collection("users").document(String(Auth.auth().currentUser!.uid)).getDocument { (document, error) in
+            if let document = document, document.exists {
+                let uemail = document.data()?["email"] as! String
+                let ufb = document.data()?["facebook"] as! String
+                let ufirstname = document.data()?["firstName"] as! String
+                let uid = document.data()?["id"] as! String
+                let uinstagram = document.data()?["instagram"] as! String
+                let ulastname = document.data()?["lastName"] as! String
+                let uphonenumber = document.data()?["phoneNumber"] as! String
+                let usnapchat = document.data()?["snapchat"] as! String
+                let utwitter = document.data()?["twitter"] as! String
+                let utwowayswap = document.data()?["twoWaySwap"] as! Bool
+                let uswapreceives = document.data()?["userNamesOfSwapRecieves"] as! [String]
+                self.user = User(uid: uid, firstName: ufirstname, lastName: ulastname, email: uemail, phoneNumber: uphonenumber, twitter: utwitter, instagram: uinstagram, facebook: ufb, snapchat: usnapchat, twoWaySwap: utwowayswap, userNamesOfSwapRecieves: uswapreceives)
+                completion(self.user)
+                self.viewDidLoad()
+            } else {
+                print("Error getting user")
+                completion(nil)
             }
         }
-       
-       override func viewWillAppear(_ animated: Bool) {
-
-           getSignedInUser(completion: { user in
-               self.dbloaded=true
-                 })
-
-           
-       }
-            
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        getSignedInUser(completion: { user in
+            self.dbloaded=true
+        })
+        let test = ",,,"
+        let parts = test.components(separatedBy: ",")
+        print("parts are: ")
+        print(parts)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         
         // Do any additional setup after loading the view.
         self.delegate = self
     }
     
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
         if segue.identifier == "scannerSegue"{ //send user object to scanner
-                    let vc = segue.destination as! ScannerViewController
-                        vc.user=user
-                    }
+            let vc = segue.destination as! ScannerViewController
+            vc.user=user
+        }
         else if segue.identifier == "generateSegue"{
-                    let vc = segue.destination as! GenerateViewController
-                        vc.user=user
-                    }
+            let vc = segue.destination as! GenerateViewController
+            vc.user=user
+        }
         else if segue.identifier == "notifSegue"{
-                    let vc = segue.destination as! NotificationsTableViewController
-                        vc.user=user
-                    }
+            let vc = segue.destination as! NotificationsTableViewController
+            vc.user=user
+        }
         else if segue.identifier == "settingsSegue"{
-                    let vc = segue.destination as! SettingsViewController
-                        vc.user=user
-                    }
-     }
-
+            let vc = segue.destination as! SettingsViewController
+            vc.user=user
+        }
+    }
     
-
-}
+    
+    
+ }
